c93e2fdc64886c7ea5e399fbd03b0f6e83d0f905
rajateja6 <rajateja6@gmail.com>
2025-08-05 09:57:12 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -5,3 +5,4 @@ bool isPulseOk(float pulseRate);
 bool isSpO2Ok(float spo2);
 int vitalsOk(float temperature, float pulseRate, float spo2);
 void showAlert(const std::string& message);
+void blinkAlarm(int times = 6);
