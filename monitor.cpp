51278a7e17e6f6982c82469b124b3f8e6d3d9eb3
rajateja6 <rajateja6@gmail.com>
2025-08-05 09:29:50 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,37 +1,48 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
+
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+bool isTemperatureOk(float temperature) {
+  return temperature >= 95 && temperature <= 102;
+}
+
+bool isPulseOk(float pulseRate) {
+  return pulseRate >= 60 && pulseRate <= 100;
+}
+
+bool isSpO2Ok(float spo2) {
+  return spo2 >= 90;
+}
+
+void blinkAlarm(int times) {
+  for (int i = 0; i < times; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+  cout << "\n";
+}
+
+void showAlert(const std::string& message) {
+  cout << message << "\n";
+  blinkAlarm();
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  if (!isTemperatureOk(temperature)) {
+    showAlert("Temperature is critical!");
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  }
+  if (!isPulseOk(pulseRate)) {
+    showAlert("Pulse Rate is out of range!");
     return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  }
+  if (!isSpO2Ok(spo2)) {
+    showAlert("Oxygen Saturation out of range!");
     return 0;
   }
   return 1;
