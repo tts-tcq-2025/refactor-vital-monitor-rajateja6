35fa9ab394ae88d5bc462901e8a703a39986049c
rajateja6 <rajateja6@gmail.com>
2025-08-05 10:12:09 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,10 +1,10 @@
 #pragma once
-#include <string> 
-
+#include <string>
 bool isTemperatureOk(float temperature);
 bool isPulseOk(float pulseRate);
 bool isSpO2Ok(float spo2);
 int vitalsOk(float temperature, float pulseRate, float spo2);
 void showAlert(const std::string& message);
 void blinkAlarm(int times = 6);
+
 
