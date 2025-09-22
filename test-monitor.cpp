52b9f87fbdc304dc462a86811eefacf558c7f93f
rajateja6 <rajateja6@gmail.com>
2025-09-22 14:54:03 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,19 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(Monitor, VitalChecks) {
+  VitalStatus status1 = checkVitals(99, 102, 70);
+  EXPECT_FALSE(status1.pulseOk);
+  EXPECT_FALSE(status1.spo2Ok);
+  EXPECT_TRUE(status1.temperatureOk);
+
+  VitalStatus status2 = checkVitals(98.1, 70, 98);
+  EXPECT_TRUE(status2.temperatureOk);
+  EXPECT_TRUE(status2.pulseOk);
+  EXPECT_TRUE(status2.spo2Ok);
+
+  VitalStatus status3 = checkVitals(88, 50, 85);
+  EXPECT_FALSE(status3.temperatureOk);
+  EXPECT_FALSE(status3.pulseOk);
+  EXPECT_FALSE(status3.spo2Ok);
 }
