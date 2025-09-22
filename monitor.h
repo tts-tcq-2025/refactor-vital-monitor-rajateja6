b74060c2553759bae627be41940991610462a82c
rajateja6 <rajateja6@gmail.com>
2025-09-22 14:52:42 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <string>
+
+struct VitalStatus {
+  bool temperatureOk;
+  bool pulseOk;
+  bool spo2Ok;
+};
+
+VitalStatus checkVitals(float temperature, float pulseRate, float spo2);
+void showAlert(const VitalStatus& status);
+void blinkAlarm(int times = 6);
+
 bool isTemperatureOk(float temperature);
 bool isPulseOk(float pulseRate);
 bool isSpO2Ok(float spo2);
-int vitalsOk(float temperature, float pulseRate, float spo2);
-void showAlert(const std::string& message);
-void blinkAlarm(int times = 6);
-
-
